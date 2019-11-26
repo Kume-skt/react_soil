03ae372a5d3f3cc5a2a8e605d951c7550d6d9009
Kume-skt <56424792+Kume-skt@users.noreply.github.com>
2019-11-27 00:46:33 +0900

    tenki
    サンプル表示

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <h1>よろしく</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {/* ****** ここから onClick を追加 ****** */}
+          <a onClick={this.handleClick()}>天気を表示</a>
+          {/* ****** ここまで追加 ****** */}
+          {/* ****** ここに天気をだす ****** */}
+          <p>{this.state.tenki}</p>
+          {/* ****** ここまで追加 ****** */}
+          <form>
+            <label>
+              Name:
+              <input type="text" name="name" />
+            </label>
+            <button  type="submit" value="Submit" />
+          </form>
+        </header>
+      </div>
+    );
+  }
+  // ***** クリック時呼び出される関数を追加 *****
+  handleClick() {
+    this.setState({ tenki: "晴れ(仮)" });
+  }
+  // ***** ここまで追加 *****
+  // ***** コンストラクタ追加して初期値の設定 *****
+  constructor(props) {
+    super(props);
+    this.state = { tenki: "まだ天気とってません" };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  // ***** ここまで追加 *****
 }
 
 export default App;
